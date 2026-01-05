b00fb18b1b748cb32ec6d0b228e3eb4bc00e6709
Eri Yana <120174018+ameriyana@users.noreply.github.com>
2026-01-05 15:29:55 +0700

    Update MenuItemSeeder.php

--- database/seeders/MenuItemSeeder.php
+++ database/seeders/MenuItemSeeder.php
@@ -1,26 +1,26 @@
-<?php
+// <?php
 
-namespace Database\Seeders;
+// namespace Database\Seeders;
 
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use App\Models\Menu;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Seeder;
+// use App\Models\Menu;
 
-class MenuItemSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        Menu::create([
-            'nama_menu' => 'Nori',
-            'foto_menu' => 'img/Dimsumdisplay6.png',
-            'harga_reguler' => 15000,
-            'harga_mini' => 12000,
-            'kategori' => 'reguler',
-            'stock' =>  15
-        ]);
-    }
-}
+// class MenuItemSeeder extends Seeder
+// {
+//     /**
+//      * Run the database seeds.
+//      */
+//     public function run(): void
+//     {
+//         Menu::create([
+//             'nama_menu' => 'Nori',
+//             'foto_menu' => 'img/Dimsumdisplay6.png',
+//             'harga_reguler' => 15000,
+//             'harga_mini' => 12000,
+//             'kategori' => 'reguler',
+//             'stock' =>  15
+//         ]);
+//     }
+// }
